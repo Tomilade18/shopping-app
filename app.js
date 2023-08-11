570536bf9d8374a0f7f789a2e172026513926857
Tomilade18 <jboyyumzey@gmail.com>
2023-08-11 02:28:43 +0100

    new

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const search = () =>{
 const searchbox = document.getElementById('search-item').value.toUpperCase();
 const storeitems = document.getElementById('product-list')
 const product = document.querySelectorAll(".product")
-const pname = document.getElementsByTagName("h2")
+const pname = storeitems.getElementsByTagName("h2")
 
     for(var i=o; i < pname.length, i++){
         let match = product[i].getElementsByTagName('h2')[0];
 
         if(match){
-          let = match.textContent || match.innerHTML
+          let textvalue = match.textContent || match.innerHTML
+
+          if(textvalue.toUpperCase().indexOf(searchbox) > -1) {
+            product[i].style.display = "";
+          }else{
+            product[i].style.display = "none";
+          }
         }
     }
 }
