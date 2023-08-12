5996cdda821d23cd10e4b5c9fe5f154465075dd1
Tomilade18 <jboyyumzey@gmail.com>
2023-08-12 20:51:42 +0100

    new

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const search = () =>{
-const searchbox = document.getElementById('search-item').value.toUpperCase();
+const searchbox = document.getElementById('search-items').value.toUpperCase();
 const storeitems = document.getElementById('product-list')
 const product = document.querySelectorAll(".product")
 const pname = storeitems.getElementsByTagName("h2")
 
-    for(var i=o; i < pname.length, i++){
+    for (var i = 0; i < pname.length; i++) {
         let match = product[i].getElementsByTagName('h2')[0];
 
-        if(match){
+        if (match){
           let textvalue = match.textContent || match.innerHTML
 
-          if(textvalue.toUpperCase().indexOf(searchbox) > -1) {
+          if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
             product[i].style.display = "";
-          }else{
+          } else {
             product[i].style.display = "none";
           }
         }
